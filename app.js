09a6d4c2f30ee82cb20b9705de5da7de6414b851
zaidfaisal <mzaidfaisal09@gmail.com>
2024-09-05 16:45:21 +0500

    add files and folder

---
+++ app.js
@@ -0,0 +1,135 @@
+// chapter 1 alerts
+// Q1
+// alert( )
+// Q2
+// alert( "Error! Please enter a valid Password")
+// Q3
+// alert("Welcome to JS Land... \n Happy Coding")
+// Q4
+//    alert("Welcome to JS Land...")
+//    alert("Welcome to JS Land... \n Happy Coding \n Prevents this page from additional setting")                                                                                                                                                                                                                              
+// Q5
+// console.log("Hello... i can run JS through my web developer console")
+// Q6
+// alert( )
+// Q7
+// answer is in the index file
+
+// chapter 2 string for variable
+// Q1
+// var username;
+// Q2
+// var myname="zaidfaisal";
+// console.log(myname)
+// Q3
+// var message="Hello World";
+// alert(message)
+// Q4
+// alert("Jhone Doe")
+// alert("15 years old")
+// alert("Certificated Mobile Application Development")
+// Q5
+// alert("Pizza \n Pizz \n Piz \n Pi \n P")
+// Q6
+// var email ="mzaidfaisal09@gmail.com"
+// alert("My Email Address is " + email)
+// Q7
+// var message=("I am trying to learn Javascript from book A smarter way to learn")
+// alert(message)
+// Q8
+// document.write("Yah! i can write HTML code through JavaScript")
+// Q9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+// chapter 3 string for number
+// Q1
+// var age=15;
+// alert("I am " + age + " years old" )
+// Q2
+// var visitor=14;
+// alert("You have visited this site " + visitor + " times" )
+// Q3
+// var birthyear=1990;
+// alert("My birthyear is " + birthyear + "\n Datatype of my declare variable is number")
+// Q4
+// var visitorname="John Doe"
+// var ProductTitle="T-shirt"
+// var Quantity=5
+// document.write(visitorname + " Ordered " + Quantity + ProductTitle + "(s) on XYZ Cloting Store" )
+
+// chapter 4 legal and illegal
+// Q1
+// var a=1, b=2, c=3
+// Q2
+// legall variables
+// var firstName
+// var _count
+// var age2
+// var totalAmount
+// var $price
+// illegal variables
+// var 2ndPlace 
+// var first-name 
+// var var 
+// var total amount 
+// var @username
+// Q3
+// document.write("Rules for naming JS variables  <br>  <br>  <br> <br>  1. Variable names can only contain letters, numbers, underscores, and dollar signs.  For example: $my_1stVariable  <br> 2. Variables must begin with a letter, underscore (_), or dollar sign ($). For example: $name, _name, or name  <br> 3. Variable names are case sensitive.  <br> 4. Variable names should not be JS keywords.");
+
+
+
+
+
+
+// chapter1 alert
+// Q1
+// alert("FirstName")
+// alert("LastName")
+// alert("Email")
+// alert("Phone Number")
+// alert("Password")
+// Q2
+// alert("You're learning JavaScript")
+// Q3
+// alert("I am learning JavaScript")
+
+
+// chapter2 variable for string
+
+// Q1
+// var userName=zaid;
+// Q2
+// var message;
+// var message="Hello world"
+// Q3
+// var TeamName="XYZ"
+// alert(TeamName)
+// Q4
+// var bestMan="Charlie"
+// bestMan="Ali"
+// alert(bestMan)
+
+// chapter variable for Number
+
+// Q1
+// var caseQty;
+// Q2
+// caseQty=144;
+// Q3
+// var numm=9
+// var total=numm + 5
+// console.log(total)
+// Q4
+// var num=10;
+// console.log(num)
+// Q5
+// var merchantTotal=100
+// var shippingCharge=10
+// var orderTotal=merchantTotal + shippingCharge
+// console.log(orderTotal)
+// Q6
+// var mynum=2
+// mynum=4
+// console.log(mynum)
+
